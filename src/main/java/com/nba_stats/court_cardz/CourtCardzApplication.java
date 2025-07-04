7a18f65207ce712c0700daebd10138beef2edc1b
JashawnRogers <rogersjashawn20@gmail.com>
2025-07-03 22:36:12 -0700

    First commit

---
+++ src/main/java/com/nba_stats/court_cardz/CourtCardzApplication.java
@@ -0,0 +1,13 @@
+package com.nba_stats.court_cardz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtCardzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourtCardzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nba_stats/court_cardz/CourtCardzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nba_stats.court_cardz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourtCardzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
